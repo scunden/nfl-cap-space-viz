af6d4cd3fd4422d87fc2354b6ce335ec0728559d
Steven Cunden <slcunden@gmail.com>
2022-11-29 16:35:53 -0500

    initial commit

---
+++ scraper.py
@@ -0,0 +1,101 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import numpy as np
+import re
+import pickle
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG, format='%(asctime)s %(message)s', 
+    handlers=[
+        logging.FileHandler("scraping_logs.log"),
+        logging.StreamHandler()
+    ],
+    datefmt='%m/%d/%Y %I:%M:%S %p')
+
+MAIN = 'https://www.spotrac.com/nfl/'
+
+def get_teams_cap_url():
+    
+    response = requests.get(MAIN)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    urls = []
+    
+    logging.info('Scraping team URLs...')
+    for a in soup.find_all("a", href=True):
+        if a['href'].endswith('/cap/') and 'nfl' in a['href'] and a['href']!='https://www.spotrac.com/nfl/cap/':
+            logging.debug('Appending {}'.format(a['href']))
+            urls.append(a['href'])
+
+    urls = list(set(urls))
+    logging.info('Scraping team URLs done.')
+
+    return urls
+
+def scrape_player_details(soup, year, team):
+    logging.debug('Scraping player details for the {} {}...'.format(year, team))
+    df = pd.read_html(str(soup.find_all("table")[0]))[0]
+    df['Year'] = year
+    df['Team'] = team
+    active = [x for x in df.columns if 'Active' in x][0]
+    df.rename({active:'Player Name'}, axis=1, inplace=True)
+    if 'Cap Hit.1' in df.columns:
+        df.drop(['Cap Hit.1'], axis=1, inplace=True)
+    return df
+
+def scrape_team_details(soup, year, team):
+    logging.debug('Scraping team details for the {} {}...'.format(year, team))
+    tag = soup.find_all('section',{'class':'module-singles xs-hide'})[0]
+    metrics = [x.text.strip(':') for x in tag.find_all('span',{'class':'info'})]
+    values = [float(x.text.replace('$','').replace(',','')) if x.text!= '-' else 0 for x in tag.find_all('a') ]
+    data = {metrics[i]:[values[i]] for i in range(len(metrics))}
+    data.update({'Team':[team],'Year':[year]})
+    return pd.DataFrame(data= data)
+
+def scrape_data():
+    teams_url = get_teams_cap_url()
+    player_details = pd.DataFrame()
+    team_details_22 = pd.DataFrame()
+    team_details_23 = pd.DataFrame()
+
+    for url in teams_url:
+        for year in [2022, 2023]:
+            url = url+'2023/' if year == 2023 else url
+            team = re.search('nfl/(.*)/cap/', url).group(1).replace('-',' ').title()
+            logging.info('Scraping the {} {}...'.format(year, team))
+
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            team_detail = scrape_team_details(soup, year, team)
+            player_detail = scrape_player_details(soup, year, team)
+
+            player_details = pd.concat([player_details, player_detail])
+            if year == 2023:
+                team_details_23 = pd.concat([team_details_23, team_detail])
+            else:
+                team_details_22 = pd.concat([team_details_22, team_detail])
+
+    return player_details, team_details_22, team_details_23
+
+def main():
+    player_details, team_details_22, team_details_23 = scrape_data()
+
+    logging.info('Exporting player_details')
+    with open('data/player_details.pickle', 'wb') as handle:
+        pickle.dump(player_details, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    logging.info('Exporting team_details_22')
+    with open('data/team_details_22.pickle', 'wb') as handle:
+        pickle.dump(team_details_22, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        
+    logging.info('Exporting team_details_23')
+    with open('data/team_details_23.pickle', 'wb') as handle:
+        pickle.dump(team_details_23, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+# with open('filename.pickle', 'rb') as handle:
+#     b = pickle.load(handle)
+
+if __name__=='__main__':
+    main()
